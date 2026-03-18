f49ab91c2ff7a531dba27252cdc23db393345e83
kyut0 <katy.yut@gmail.com>
2026-03-18 11:38:51 -0500

    Update __main__.py

--- habit_tracking/__main__.py
+++ habit_tracking/__main__.py
@@ -7,12 +7,7 @@ import argparse
 def main(ssid: str, creds: str, sleep_file: str, weight_file: str):
     tracker = HabitTracker()
 
-    tracker.apply_cleaning(
-        service_account_file=creds,
-        spreadsheet_id=ssid,
-        sleep_file=sleep_file,
-        weight_file=weight_file
-    )
+    tracker.load_and_clean()
     
     print("Success!")
 
@@ -20,8 +15,8 @@ def main(ssid: str, creds: str, sleep_file: str, weight_file: str):
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description="Run the habit tracking application.")
-    parser.add_argument("--ssid", type=str, required=True, help="Google Drive spreadsheet ID.")
-    parser.add_argument("--creds", type=str, required=True, help="Path to the Google Drive service account credentials JSON file.")
+    parser.add_argument("--ssid", type=str, required=False, help="Google Drive spreadsheet ID.")
+    parser.add_argument("--creds", type=str, required=False, help="Path to the Google Drive service account credentials JSON file.")
     parser.add_argument("--sleep", type=str, required=False, help="Path to the sleep data CSV file.")
     parser.add_argument("--weight", type=str, required=False, help="Path to the weight data CSV file.")
     
